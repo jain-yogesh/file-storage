d8fd936a77cf3243c53e3c8a560b3244dddc2a17
Yogesh <nic.helpu@gmail.com>
2024-11-19 17:06:56 +0530

    add sign in functionality

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -117,10 +117,27 @@ export const verifySecret = async ({
     try {
       await account.deleteSession("current");
       (await cookies()).delete("appwrite-session");
-      
+
     } catch (error) {
       handleError(error, "Failed to sign out user");
     } finally {
       redirect("/sign-in");
+    }
+  };
+
+  export const signInUser = async ({ email }: { email: string }) => {
+    try {
+      const existingUser = await getUserByEmail(email);
+  
+      // User exists, send OTP
+      if (existingUser) {
+        await sendEmailOTP({ email });
+        return parseStringify({ accountId: existingUser.accountId });
+      }
+  
+      return parseStringify({ accountId: null, error: "User not found !!" });
+
+    } catch (error) {
+      handleError(error, "Failed to sign in user");
     }
   };
