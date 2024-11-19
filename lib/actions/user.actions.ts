4831e8a49006eff226244005a8127e927e11f2e8
Yogesh <nic.helpu@gmail.com>
2024-11-19 13:16:19 +0530

    add user page sidebar

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,10 +1,11 @@
 "use server";
 
 import { ID, Query } from "node-appwrite";
-import { createAdminClient } from "../appwrite";
+import { createAdminClient, createSessionClient } from "../appwrite";
 import { appwriteConfig } from "../appwrite/config";
 import { parseStringify } from "../utils";
 import { cookies } from "next/headers";
+import { avatarPlaceholder } from "@/constants";
 
 const getUserByEmail = async (email: string) => {
     const { databases } = await createAdminClient();
@@ -52,7 +53,7 @@ export const createAccount = async ({fullName, email } : {fullName : string; ema
             {
                 fullName,
                 email,
-                avatar: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png",
+                avatar: avatarPlaceholder,
                 accountId,
             },
         );
@@ -84,5 +85,27 @@ export const verifySecret = async ({
 
     } catch (error) {
       handleError(error, "Failed to verify OTP");
+    }
+  };
+
+  export const getCurrentUser = async () => {
+    try {
+      const { databases, account } = await createSessionClient();
+  
+      const result = await account.get();
+  
+      const user = await databases.listDocuments(
+        appwriteConfig.databaseId,
+        appwriteConfig.usersCollectionId,
+        [Query.equal("accountId", result.$id)],
+      );
+  
+      if (user.total <= 0) return null;
+  
+      return parseStringify(user.documents[0]);
+
+    }
+    catch (error) {
+      console.log(error);
     }
   };
