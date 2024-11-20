15f5ead423da676c82d6741116da29ba9e82a136
Yogesh <nic.helpu@gmail.com>
2024-11-20 12:52:08 +0530

    add file upload functionality

--- components/MobileNavigation.tsx
+++ components/MobileNavigation.tsx
@@ -18,14 +18,14 @@ import FileUploader from "./FileUploader";
 import { signOutUser } from "@/lib/actions/user.actions";
 
 interface myAppProps {
-  ownerId: string;
+  $id: string;
   accountId: string;
   fullName: string;
   avatar: string;
   email: string;
 }
 
-const MobileNavigation = ({ownerId, accountId, fullName, avatar, email} : myAppProps) => {
+const MobileNavigation = ({$id: ownerId, accountId, fullName, avatar, email} : myAppProps) => {
   const [open, setOpen] = useState(false);
   const pathname = usePathname();
 
@@ -65,7 +65,7 @@ const MobileNavigation = ({ownerId, accountId, fullName, avatar, email} : myAppP
             </nav>
             <Separator className="my-5 bg-light-200/20" />
             <div className="flex flex-col justify-between gap-5 pb-5">
-                <FileUploader />
+                <FileUploader ownerId={ownerId} accountId={accountId} />
                 <Button type='submit' className='mobile-sign-out-button' onClick={async () => await signOutUser()}>
                     <Image src={"assets/icons/logout.svg"} alt='sign-out' width={24} height={24} />
                     <p>Logout</p>
