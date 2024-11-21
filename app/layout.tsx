0260ec3643e3477a7844cfee6508d6fdd63474df
Yogesh <nic.helpu@gmail.com>
2024-11-21 13:33:44 +0530

    change title

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: "File Store",
+  title: "File Storage",
   description: "The only solution for your file storage",
 };
 
